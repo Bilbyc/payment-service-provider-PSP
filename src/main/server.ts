b5e4f2d57772103e6fa09cadb8e218b3d43e609f
Carlos Bilby <carlos.bilby@gmail.com>
2023-06-20 19:05:09 -0300

    chore: add and configure lint

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,16 +1,15 @@
-import express from 'express'
-import { Router, Request, Response } from 'express'
+import express, { Router, Request, Response } from 'express'
 
-const app = express();
+const app = express()
 
 const route = Router()
 
 app.use(express.json())
 
 route.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'PSP project'})
+  res.json({ message: 'PSP project' })
 })
 
 app.use(route)
 
-app.listen(3333, () => console.log('server running on port 3333'))
+app.listen(3333, () => { console.log('server running on port 3333') })
