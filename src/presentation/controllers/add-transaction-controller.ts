524886d243156a0f673a55d086930d131a272ccf
Carlos Bilby <carlos.bilby@gmail.com>
2023-06-20 23:58:00 -0300

    feat: add controller initial structure

---
+++ src/presentation/controllers/add-transaction-controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from '../protocols/controller'
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
+export class AddTransactionController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    return {
+      statusCode: 400,
+      body: { }
+    }
+  }
+}
